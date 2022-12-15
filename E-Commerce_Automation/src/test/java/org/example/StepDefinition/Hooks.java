3868a5c889271c866443a9effdb493a799c28785
Mohamed Rizk <110140601+Rizk0@users.noreply.github.com>
2022-12-15 10:44:53 +0200

    Update Hooks.java

--- E-Commerce_Automation/src/test/java/org/example/StepDefinition/Hooks.java
+++ E-Commerce_Automation/src/test/java/org/example/StepDefinition/Hooks.java
@@ -1,33 +1,44 @@
+
 package org.example.StepDefinition;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+        import io.cucumber.java.After;
+        import io.cucumber.java.Before;
+        import io.github.bonigarcia.wdm.WebDriverManager;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
+        import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
-    public static WebDriver driver = null ;
+    // Declration Webdriver
+    public static WebDriver driver = null;
+
 
     @Before
-    public void Open_Browser_Navigate_Website()
-    {
-        String chromePath = System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver" , chromePath);
+    public static void openbrowser() {
 
-        driver = new ChromeDriver() ;
+        // 1- Define Bridge WebDriver Manager
+        WebDriverManager.chromedriver().setup();
 
+        // 2- create object from chrome browser
+        driver = new ChromeDriver();
+
+        //3- Configurations
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(8 , TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        driver.navigate().to("https://demo.nopcommerce.com/");
+        // 4- navigate to url
+        driver.get("https://demo.nopcommerce.com/");
+
+
     }
 
     @After
-    public void Close_Browser()
-    {
+    public static void quitDriver() throws InterruptedException {
+        Thread.sleep(3000);
         driver.quit();
+
+
     }
 }
